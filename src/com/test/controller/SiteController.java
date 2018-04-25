e685c1574bcb551ee1c3c8d049076fcf09247eed
nidheeshmathew <nidheeshmathew2010@gmail.com>
2018-04-25 11:31:46 +0530

    first commit

---
+++ src/com/test/controller/SiteController.java
@@ -0,0 +1,20 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SiteController 
+{
+	@RequestMapping("/")
+	public String index()
+	{
+		return "index";
+	}
+	@RequestMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+
+}
